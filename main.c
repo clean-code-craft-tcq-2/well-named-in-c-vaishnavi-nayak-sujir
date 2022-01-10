951851ca3d4dba190ef9607f27621e860d281647
vaishnavi-nayak-sujir <vaishnavinayak86@gmail.com>
2022-01-10 17:39:33 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,6 @@
 #include "modularity.h"
 #include "test_pair_number.h"
 
-
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
-}
-
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
