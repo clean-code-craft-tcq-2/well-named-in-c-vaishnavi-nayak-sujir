d062d58bdae86e3508611cc4ac2be85c4e704353
vaishnavi-nayak-sujir <vaishnavinayak86@gmail.com>
2022-01-10 18:03:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,22 +2,7 @@
 #include <assert.h>
 #include "modularity.h"
 #include "test_colorcode.h"
-
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
-
+#include "get_colorcode.h"
 
 int main() {
     testNumberToPair(4, WHITE, BROWN);
