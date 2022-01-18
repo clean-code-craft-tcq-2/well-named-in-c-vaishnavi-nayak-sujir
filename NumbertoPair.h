615b69cd160223c0ee988335d178581ad9d50f0e
vaishnavi-nayak-sujir <vaishnavinayak86@gmail.com>
2022-01-18 17:24:46 +0530

    Update NumbertoPair.h

--- NumbertoPair.h
+++ NumbertoPair.h
@@ -1,1 +1,6 @@
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
 
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor);
