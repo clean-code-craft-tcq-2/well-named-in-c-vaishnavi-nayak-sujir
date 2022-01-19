f936e0622b4221043a90e167ca80876281bb2908
vaishnavi-nayak-sujir <vaishnavinayak86@gmail.com>
2022-01-19 15:13:08 +0530

    Update NumbertoPair.c

--- NumbertoPair.c
+++ NumbertoPair.c
@@ -1,6 +1,5 @@
-#include "NumbertoPair.h"
 #include "modularity.h"
-
+#include <assert.h>
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
@@ -30,3 +29,27 @@ void testNumberToPair(int pairNumber,
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
 }
+
+void printmanual()
+
+{ 
+	int pairnum_loop_cntr;
+	int total_pairs= numberOfMajorColors * numberOfMinorColors;
+	printf("**************Manual***************\n");
+	printf("Pair no.\tMajor color\tMinor color\n");
+	for (pairnum_loop_cntr=1; pairnum_loop_cntr<= total_pairs; pairnum_loop_cntr++)
+	{		
+		
+			ColorPair colorPair = GetColorFromPairNumber(pairnum_loop_cntr);
+			char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+			
+			printf("%d\t\t%s\t\t%s\n", pairnum_loop_cntr,MajorColorNames[colorPair.majorColor],MinorColorNames[colorPair.minorColor]);
+	}
+}	
+
+
+
+
+
+	
+	
